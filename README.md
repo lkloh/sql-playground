49391c9c2c1d29d2eea4807b75e093fd7129e30b
LK Loh <lkloh2410@gmail.com>
2017-10-11 18:07:58 -0700

    allow print to debug

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sql-playground
 test sql scripts here
+
+Tutorials:
+* [Updating multiple rows by multiple conditions](https://stackoverflow.com/questions/25674737/mysql-update-multiple-rows-with-different-values-in-one-query)
--- playground.py
+++ playground.py
@@ -1,11 +1,21 @@
 import sqlite3
 
+
+
 def create_connection(db_file):
     """ create a database connection to a SQLite database """
     try:
     	conn = sqlite3.connect(db_file)
     	c = conn.cursor()
-    	c.execute("CREATE TABLE IF NOT EXISTS stocks (firstname text, lastname text, salary int, bonus int)")
+    	c.execute('''
+    		CREATE TABLE IF NOT EXISTS stocks 
+    		(
+    			firstname text,
+    			lastname text,
+    			salary int,
+    			bonus int,
+    			PRIMARY KEY(firstname, lastname)
+    		)''')
     	return conn
     except:
     	raise
@@ -14,14 +24,77 @@ def close_connection(conn):
     conn.close()
 
 
-def insert_row(conn):
-	c = conn.cursor
-	c.execute('''INSERT INTO stocks VALUES ('John', 'Doe', 1000, 15)''')
 
 def experiment(conn):
-	pass
+	insert_row(conn)
+	update_pay_by_first_name(conn)
+
+def insert_row(conn):
+	c = conn.cursor()
+	c.execute('''INSERT INTO stocks VALUES ('John', 'Doe', 100, 15)''')
+	c.execute('''INSERT INTO stocks VALUES ('Jane', 'Roe', 110, 5)''')
+	c.execute('''INSERT INTO stocks VALUES ('Alice', 'Anderson', 80, 20)''')
+	c.execute('''INSERT INTO stocks VALUES ('Bob', 'Brown', 70, 30)''')
+	conn.commit()
+	print_db(conn)
+
+def print_db(conn):
+	c = conn.cursor()
+	db = c.execute('''
+		SELECT * 
+		FROM stocks
+	''')
+	print '\n************************************'
+	for row in db:
+		print row
+	print '************************************\n'
+
+def update_pay_by_first_name(conn):
+	c = conn.cursor()
+	update_list = [
+		('John', 'Doe', 120, 20),
+		('Alice', 'Anderson', 95, 20),
+		('Bob', 'Brown', 85, 30)
+	]
+	c.execute('''
+		INSERT INTO stocks (firstname, lastname, salary, bonus)
+		VALUES
+			('John', 'Doe', '120', '20'),
+			('Alice', 'Anderson', '95', '20'),
+			('Bob', 'Brown', '85', '30')
+		ON DUPLICATE KEY UPDATE
+			salary=VALUES(salary), bonus=VALUES(bonus)
+	''')
+	conn.commit()
+
+
 
 if __name__ == '__main__':
 	conn = create_connection('example.db')
 	experiment(conn)
 	close_connection(conn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
